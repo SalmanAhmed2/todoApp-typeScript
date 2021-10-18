59e4fa0f2859c074de2810b87fc5c680e1b687a9
Salman Ahmed <salmanahmed35518@gmail.com>
2021-10-18 19:58:03 +0500

    edit function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import './App.css';
 import Buttons from './Components/Button';
 import Input from './Components/Input';
@@ -7,28 +7,40 @@ import TodosList from "./Components/TodosList";
 function App() {
   const [value, setValue] = useState("")
   const [todos, setTotos] = useState<{ key: string, value: string }[]>([])
-
+  const [isEdit, setEdit] = useState(false)
   const submitData = () => {
     setTotos((prev) => [...prev, { key: Math.floor(Math.random() * 100).toString(), value: value }])
     setValue("")
+    // let obj = [...todos, { key: Math.floor(Math.random() * 100).toString(), value: value }]
+    // localStorage.setItem("todos", JSON.stringify(obj))
   }
   const deleteTodo = (id: string) => {
     let filteredTodo = todos.filter((item) => item.key !== id && item)
     setTotos(filteredTodo)
   }
-  // const editTodo = (id: string) => {
-  //   let filteredArray = todos.filter((items) => items.key === id ? items.value = value : items.value)
-   
-  // }
+  const editTodo = (item: {}) => {
+    // console.log(item)
+    setEdit(true)
+    // setID(item?.key)
+  }
+  const updateTodo = (value: { key: string, value: string }) => {
+    let filteredArray = todos.filter((items) => items?.key !== value?.key && items)
+    // let obj = filteredArray = value;
+    // setTotos((prev)=>[...prev,{key:filteredArray.keys,value:filteredArray.values}])
+  }
+  // useEffect(() => {
+  //   const data = localStorage.getItem("todos")
+  //   setTotos(data?.map((item)=>))
+  // }, [])
   return (
     <div className="App">
       <h2>Todo List</h2>
       <div style={{ display: "flex", alignItems: "center" }}>
         <Input value={value} handleChange={(event) => setValue(event.target.value)} placeholder="Title" />
         <Buttons text="Submit" handleSubmit={submitData} /></div>
-      <TodosList todosList={todos} handleDelete={deleteTodo} 
-      // handleUpdate={editTodo}
-       />
+      <TodosList todosList={todos} handleDelete={deleteTodo} editTodo={editTodo} isEdit={isEdit}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 }
--- src/Components/TodosList.tsx
+++ src/Components/TodosList.tsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import CancelIcon from '@mui/icons-material/Cancel';
-// import BorderColorIcon from '@mui/icons-material/BorderColor';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import SystemUpdateAltIcon from '@mui/icons-material/SystemUpdateAlt';
+import { Input } from "@mui/material";
+
 type listDataProps = {
     todosList: {
         key: string,
         value: string
     }[],
     handleDelete: (id: string) => void,
-    // handleUpdate: (id: string) => void
+    editTodo: (item: {
+        key: string,
+        value: string
+    }) => void,
+    updateTodo: (item: { key: string, value: string }) => void,
+    isEdit: boolean
 }
-const TodosList: React.FC<listDataProps> = ({ todosList, handleDelete,
-    // handleUpdate 
+const TodosList: React.FC<listDataProps> = ({ todosList, handleDelete, editTodo, isEdit,
+    updateTodo
 }) => {
-    return (
+    const [state, setstate] = useState<{ key: string, value: string }>()
+      return (
         <ul>
             {todosList.map((item, key) => {
                 return (
                     <div style={{ display: "flex" }} key={key}>
-                        <li style={{ listStyle: "none" }} key={key}>{item?.value}</li>
+                        <li style={{ listStyle: "none" }} key={key}>
+                            {isEdit && item?.key == state?.key ?
+                                <Input value={state?.value} onChange={(e) => setstate({ key: item.key, value: e.target.value })} />
+                                : item?.value}
+                        </li>
                         <li style={{ listStyle: "none", margin: "0 5px" }}><CancelIcon
                             onClick={() => handleDelete(item?.key)}
                         /></li>
-                        {/* <li style={{ listStyle: "none" }}><BorderColorIcon onClick={() => handleUpdate(item?.key)} /></li> */}
+                        <li style={{ listStyle: "none" }}>{isEdit && item?.key == state?.key ?
+                            <SystemUpdateAltIcon
+                                onClick={() => updateTodo({ key: state?.key!, value: state?.value! })
+                                }
+                            /> :
+                            <BorderColorIcon onClick={() => {
+                                editTodo({ key: item.key, value: item.value })
+                                setstate(item)
+                            }
+                            } />}</li>
                     </div>)
             })}
         </ul>
